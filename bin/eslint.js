c69aa7889943cfd7367921b73c5f8ba1ece064be
Brian Zalewski <brian@megabyte.space>
2022-02-18 10:29:09 -0500

    🔧 chore(latest): latest

--- bin/eslint.js
+++ bin/eslint.js
@@ -1,7 +1,7 @@
 #!/usr/src/app/bin/node_gc
 
-const CODE_DIR = "/code";
-process.chdir(CODE_DIR);
+const CODE_DIR = '/code'
+process.chdir(CODE_DIR)
 
-const ESLint = require("../lib/eslint");
-ESLint.run(console, { dir: CODE_DIR });
+const ESLint = require('../lib/eslint')
+ESLint.run(console, { dir: CODE_DIR })
